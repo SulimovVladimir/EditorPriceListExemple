b4de1d5a0096e1b48c840b01093e74ec8db06bbe
DESKTOP-BIKJTGM\VPSulimov <Sulimov90@gmail.com>
2024-06-19 11:42:12 +0400

    Добавьте файлы проекта.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EditorPriceListExemple.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
